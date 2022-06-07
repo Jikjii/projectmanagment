c1e87ecb37a03a81e5a8c28d1a77f36c8d10182e
Jikjii <85137475+Jikjii@users.noreply.github.com>
2022-06-07 09:26:12 -0400

    connected to db

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,10 +5,27 @@ const {
   GraphQLID,
   GraphQLString,
   GraphQLSchema,
+  GraphQLList,
 } = require("graphql");
 
-// Creating client type
+// Creating Project Type
+const ProjectType = new GraphQLObjectType({
+  name: "Project",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: { 
+        type: ClientType,
+        resolve(parent, args) {
+            return clients.find(client => client.id === parent.clientId)
+        }
+    },
+  }),
+});
 
+// Creating client type
 const ClientType = new GraphQLObjectType({
   name: "Client",
   fields: () => ({
@@ -22,6 +39,25 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(patent, args) {
+        return projects;
+      },
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parentValue, args) {
+        return projects.find((project) => project.id === args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(patent, args) {
+        return clients;
+      },
+    },
     client: {
       type: ClientType,
       args: { id: { type: GraphQLID } },
