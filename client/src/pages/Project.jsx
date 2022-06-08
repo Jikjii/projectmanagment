1c8a6ad2b2f2d7c171bdc41f5fa8909cb6ccb690
Jikjii <85137475+Jikjii@users.noreply.github.com>
2022-06-08 13:08:01 -0400

    Project Page Updated

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,16 +1,14 @@
 import { Link, useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
+import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
+import EditProjectForm from "../components/EditProjectForm";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
-import ClientInfo from "../components/ClientInfo";
 
 export default function Project() {
-  // gives us the id from the url
   const { id } = useParams();
-
-  const { loading, error, data } = useQuery(GET_PROJECT, {
-    variables: { id },
-  });
+  const { loading, error, data } = useQuery(GET_PROJECT, { variables: { id } });
 
   if (loading) return <Spinner />;
   if (error) return <p>Something Went Wrong</p>;
@@ -22,6 +20,7 @@ export default function Project() {
           <Link to="/" className="btn btn-light btn-sm w-25 d-inline ms-auto">
             Back
           </Link>
+
           <h1>{data.project.name}</h1>
           <p>{data.project.description}</p>
 
@@ -29,6 +28,10 @@ export default function Project() {
           <p className="lead">{data.project.status}</p>
 
           <ClientInfo client={data.project.client} />
+
+          <EditProjectForm project={data.project} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
